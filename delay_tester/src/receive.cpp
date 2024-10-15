a4f28172656cfc76422ff664c668ea8475d211c1
Yao-Xinchen <yao29@illinois.edu>
2024-10-14 23:22:57 -0500

    complete delay_tester

--- delay_tester/src/receive.cpp
+++ delay_tester/src/receive.cpp
@@ -1,8 +1,45 @@
 #include "rclcpp/rclcpp.hpp"
+#include <vector>
+
+#include "std_msgs/msg/float64.hpp"
+
+class Receive : public rclcpp::Node
+{
+public:
+    Receive() : Node("receive")
+    {
+        sub_ = this->create_subscription<std_msgs::msg::Float64>("forward", 10,
+            std::bind(&Receive::sub_callback, this, std::placeholders::_1));
+        pub_ = this->create_publisher<std_msgs::msg::Float64>("backward", 10);
+        RCLCPP_INFO(this->get_logger(), "Receive Initialized");
+    }
+
+    ~Receive()
+    {
+        double sum = 0;
+        for (auto& delay : delays)
+            sum += delay;
+        RCLCPP_INFO(this->get_logger(), "\033[32mAverage Single Delay: %f\033[0m", sum / delays.size());
+    }
+
+private:
+    rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr sub_;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr pub_;
+    std::vector<double> delays;
+
+    void sub_callback(const std_msgs::msg::Float64::SharedPtr msg)
+    {
+        pub_->publish(*msg);
+        auto now = rclcpp::Clock().now().seconds();
+        RCLCPP_INFO(this->get_logger(), "Single Delay: %f", now - msg->data);
+        delays.push_back(now - msg->data);
+    }
+};
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Receive>());
     rclcpp::shutdown();
     return 0;
 }
--- delay_tester/src/send.cpp
+++ delay_tester/src/send.cpp
@@ -1,8 +1,54 @@
 #include "rclcpp/rclcpp.hpp"
+#include <vector>
+
+#include "std_msgs/msg/float64.hpp"
+
+class Send : public rclcpp::Node
+{
+public:
+    Send() : Node("send")
+    {
+        pub_ = this->create_publisher<std_msgs::msg::Float64>("forward", 10);
+        timer_ = this->create_wall_timer(std::chrono::milliseconds(100),
+            std::bind(&Send::timer_callback, this));
+        sub_ = this->create_subscription<std_msgs::msg::Float64>("backward", 10,
+            std::bind(&Send::sub_callback, this, std::placeholders::_1));
+        RCLCPP_INFO(this->get_logger(), "Send Initialized");
+    }
+
+    ~Send()
+    {
+        double sum = 0;
+        for (auto& delay : delays)
+            sum += delay;
+        RCLCPP_INFO(this->get_logger(), "\033[32mAverage Double Delay: %f\033[0m", sum / delays.size());
+    }
+
+private:
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr sub_;
+    std::vector<double> delays;
+
+    void timer_callback()
+    {
+        auto msg = std_msgs::msg::Float64();
+        msg.data = rclcpp::Clock().now().seconds();
+        pub_->publish(msg);
+    }
+
+    void sub_callback(const std_msgs::msg::Float64::SharedPtr msg)
+    {
+        auto now = rclcpp::Clock().now().seconds();
+        RCLCPP_INFO(this->get_logger(), "Double Delay: %f", now - msg->data);
+        delays.push_back(now - msg->data);
+    }
+};
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Send>());
     rclcpp::shutdown();
     return 0;
 }
